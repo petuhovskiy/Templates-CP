8cb0717c56ff2a532f911ef2e0c7bce2750092b0
petuhovskiy <petuhovskiy@yandex.ru>
2015-09-14 17:10:42 +0300

    Added sieve with factorization

---
+++ fact_sieve.cpp
@@ -0,0 +1,12 @@
+const int sN = 10000000;
+int lp[sN];
+vector<int> pr;
+
+for (int i = 2; i < sN; i++) {
+	if (!lp[i]) {
+		lp[i] = i;
+		pr.pb(i);
+	}
+	for (int j = 0; j < pr.size() && pr[j] <= lp[i] && 1ll * i * pr[j] < sN; j++)
+		lp[i * pr[j]] = pr[j];
+}
