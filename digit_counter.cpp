92dc72a42cfcb8805539b8f84c4be42741bc4284
Arthur Petukhovsky <petuhovskiy@yandex.ru>
2016-01-28 00:43:55 +0300

    Create digit_counter.cpp

---
+++ digit_counter.cpp
@@ -0,0 +1,20 @@
+long long cnt[10];
+
+void count(long long r) {
+    if (r == 0) return;
+    long long tmp = r;
+    vector<int> v;
+    while (tmp > 0) v.pb(tmp % 10), tmp /= 10;
+    long long rc = 1;
+    long long lc = r / 10;
+    long long rr = 0;
+    for (int i = 0; i < v.size(); i++) {
+        cnt[0] += max(lc - 1, 0ll) * rc;
+        for (int c = 1; c < 10; c++) cnt[c] += lc * rc;
+        for (int c = (lc > 0 ? 0 : 1); c < v[i]; c++) cnt[c] += rc;
+        cnt[v[i]] += rr + 1;
+        rr += v[i] * rc;
+        rc *= 10;
+        lc /= 10;
+    }
+}
