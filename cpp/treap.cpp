8af123137aea4ff7a8b50c6bf95bc4d3c041c04f
petuhovskiy <petuhovskiy@yandex.ru>
2016-08-04 12:24:13 +0300

    treap 2.0

--- cpp/treap.cpp
+++ cpp/treap.cpp
@@ -1,34 +1,49 @@
-class treap{
+unsigned int rnd() {
+    unsigned int y = rand();
+    y <<= 16;
+    y |= rand();
+    return y;
+}
+
+struct treap{
+
     struct item{
-        int x, y, l, r, mn;
+        int x, l, r, y;
+        item(int x): x(x), l(0), r(0), y(rnd()) {}
     };
-private:
+
     vector<item> t;
-    int it = 1;
-public:
-    void resize(int num) {
-        t.resize(num);
+
+    inline item& operator[](int i) {
+        return t[i];
     }
 
-    void init(int v, int x) {
-        t[v].x = x;
-        t[v].y = rand();
-        t[v].l = t[v].r = 0;
-        t[v].mn = x;
+    inline void apply(int v/*, int add*/) {
+        //t[v].add += add;
     }
 
-    int add(int x) {
-        init(it, x);
-        return it++;
+    inline void push(int v) {
+        if (v) {
+            if (t[v].l) apply(t[v].l/*, t[v].add*/);
+            if (t[v].r) apply(t[v].r/*, t[v].add*/);
+            //t[v].add = 0;
+        }
     }
 
-    void cnt(int v) {
-        t[v].mn = min(t[v].x, min(t[t[v].l].mn, t[t[v].r].mn));
+    int add(item i) {
+        t.push_back(i);
+        return t.size() - 1;
+    }
+
+    int add(int key) {
+        return add(item(key));
     }
 
     void merge(int &v, int l, int r) {
+        push(l);
+        push(r);
         if (!l || !r) {
-            v = l ? l : r;
+            v = l | r;
         } else if (t[l].y > t[r].y) {
             v = l;
             merge(t[l].r, t[l].r, r);
@@ -36,51 +51,89 @@ public:
             v = r;
             merge(t[r].l, l, t[r].l);
         }
-        cnt(v);
     }
 
     void split(int v, int &l, int &r, int key) {
-        if (!v) {
+        if (v) {
+            push(v);
+            if (t[v].x < key) {
+                l = v;
+                split(t[l].r, t[l].r, r, key);
+            } else {
+                r = v;
+                split(t[r].l, l, t[r].l, key);
+            }
+        } else {
             l = r = 0;
-            return;
         }
-        if (t[v].x <= key) {
-            l = v;
-            split(t[v].r, t[v].r, r, key);
+    }
+
+    void insert2(int &v, int x) {
+        if (v) {
+            push(v);
+            if (t[x].y > t[v].y) {
+                split(v, t[x].l, t[x].r, t[x].x);
+                v = x;
+            } else {
+                insert(t[x].x < t[v].x ? t[v].l : t[v].r, x);
+            }
         } else {
-            r = v;
-            split(t[v].l, l, t[v].l, key);
+            v = x;
         }
-        cnt(l);
-        cnt(r);
     }
 
-    void insert(int &v, int e) {
-        if (!v) {
-            v = e;
-            return;
-        }
-        if (t[e].y > t[v].y) {
-            split(v, t[e].l, t[e].r, t[e].x);
-            v = e;
+    inline void insert(int &v, int x) {
+        if (v) {
+            int t1;
+            split(v, v, t1, t[x].x);
+            merge(v, v, x);
+            merge(v, v, t1);
         } else {
-            insert(t[e].x <= t[v].x ? t[v].l : t[v].r, e);
+            v = x;
         }
     }
 
-    int get(int x) {
-        return t[x].mn;
+    void transfer(int from, int &to) {
+        if (!from) return;
+        queue<int> q;
+        q.push(from);
+        while (!q.empty()) {
+            int v = q.front();
+            q.pop();
+            push(v);
+            if (t[v].l) q.push(t[v].l);
+            if (t[v].r) q.push(t[v].r);
+            t[v].l = t[v].r = 0;
+            insert(to, v);
+        }
     }
 
-    treap(int num) {
-        resize(num);
-        init(0, 2e9);
+    void unite(int &v, int l, int r) {
+        if (!l || !r) {
+            v = l | r;
+        } else {
+            push(l);
+            push(r);
+            if (t[l].y < t[r].y) swap(l, r);
+            int lx, rx;
+            split(r, lx, rx, t[l].x);
+            unite(t[l].l, t[l].l, lx);
+            unite(t[l].r, t[l].r, rx);
+            v = l;
+        }
     }
 
-    void output(int x) {
-        if (!x) return;
-        output(t[x].l);
-        cout << t[x].x << " ";
-        output(t[x].r);
+    template<typename F>
+    void walk(int v, F &lambda) {
+        if (v) {
+            push(v);
+            walk(t[v].l, lambda);
+            lambda(t[v]);
+            walk(t[v].r, lambda);
+        }
+    }
+
+    treap() {
+        t.push_back(item(0));
     }
 };
