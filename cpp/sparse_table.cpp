c85a483c38deb5b1be8244ec93f59f45eb9c2021
Arthur Petukhovsky <petuhovskiy@yandex.ru>
2016-10-08 11:22:38 +0300

    Update sparse_table.cpp

--- cpp/sparse_table.cpp
+++ cpp/sparse_table.cpp
@@ -5,7 +5,7 @@ struct sparse_table{
     int query(int l, int r) {
         int sz = r - l + 1;
         int b = 31 - __builtin_clz(sz);
-        return gcd(t[l][b], t[r - sz + 1][b]);
+        return max(t[l][b], t[r - (1<<b) + 1][b]);
     }
 
     sparse_table(vector<int> &v): n(v.size()), t(v.size()) {
@@ -16,7 +16,7 @@ struct sparse_table{
             int sz = 1 << e;
             int half = sz >> 1;
             for (int i = 0; i + sz <= n; i++) {
-                t[i].push_back(gcd(t[i][e - 1], t[i + half][e - 1]));
+                t[i].push_back(max(t[i][e - 1], t[i + half][e - 1]));
             }
         }
     }
