fbc77bde27a677fa09ef6808a6019f5aebad333c
Arthur Petukhovsky <petuhovskiy@yandex.ru>
2016-12-26 13:19:52 +0300

    Update maxtree.cpp

--- cpp/segment_trees/maxtree.cpp
+++ cpp/segment_trees/maxtree.cpp
@@ -1,19 +1,21 @@
-class segtree{
-public:
-    int n;  // array size
-    vector<int> t;
+template<typename T>
+struct segtree{
+    int n;
+    vector<T> t;
 
-    void build(vector<int> &v) {  // build the tree
+    void build(vector<T> &v) {
         for (int i = n; i < 2 * n; i++) t[i] = v[i - n];
         for (int i = n - 1; i > 0; --i) t[i] = max(t[i<<1], t[i<<1|1]);
     }
 
-    void modify(int p, int value) {  // set value at position p
-        for (t[p += n] = value; p > 1; p >>= 1) t[p>>1] = max(t[p], t[p^1]);
+    void modify(int p, T value) {
+        p += n;
+        if (t[p] > value) return;
+        for (t[p] = value; p > 1; p >>= 1) t[p>>1] = max(t[p], t[p^1]);
     }
 
-    int query(int l, int r) {  // sum on interval [l, r)
-        int res = 0;
+    T query(int l, int r) {
+        T res = numeric_limits<T>::min();
         for (l += n, r += n; l < r; l >>= 1, r >>= 1) {
             if (l&1) res = max(res, t[l++]);
             if (r&1) res = max(t[--r], res);
@@ -21,7 +23,5 @@ public:
         return res;
     }
 
-    segtree(int n):n(n),t(){
-        t.resize(n << 1, 0);
-    }
+    segtree(int n, T def = 0):n(n),t(n << 1, def) {}
 };
