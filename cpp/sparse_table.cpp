bde5d6d41ca0cc0cf9e3cd87c66e0e4a235e171a
petuhovskiy <petuhovskiy@yandex.ru>
2018-02-03 02:17:52 +0300

    update sparse table

--- cpp/sparse_table.cpp
+++ cpp/sparse_table.cpp
@@ -1,22 +1,24 @@
+template <typename T = int, const T& (*MX)(const T&, const T&) = std::max>
 struct sparse_table{
-    int n;
-    vector<vector<int> > t;
+    size_t n;
+    vector<vector<T>> t;
 
-    int query(int l, int r) {
-        int sz = r - l + 1;
+    // query [l, r)
+    T query(int l, int r) {
+        int sz = r - l;
         int b = 31 - __builtin_clz(sz);
-        return max(t[l][b], t[r - (1<<b) + 1][b]);
+        return MX(t[l][b], t[r - (1 << b)][b]);
     }
 
-    sparse_table(vector<int> &v): n(v.size()), t(v.size()) {
-        for (int i = 0; i < n; i++) {
+    sparse_table(const vector<T> &v): n(v.size()), t(v.size()) {
+        for (int i = 0; i < n; ++i) {
             t[i].push_back(v[i]);
         }
-        for (int e = 1; (1 << e) <= n; e++) {
+        for (int e = 1; (1 << e) <= n; ++e) {
             int sz = 1 << e;
             int half = sz >> 1;
-            for (int i = 0; i + sz <= n; i++) {
-                t[i].push_back(max(t[i][e - 1], t[i + half][e - 1]));
+            for (int i = 0; i + sz <= n; ++i) {
+                t[i].push_back(MX(t[i][e - 1], t[i + half][e - 1]));
             }
         }
     }
